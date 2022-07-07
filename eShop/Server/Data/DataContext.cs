caad7192d0a934a446a5a66ac3959a0d78f556c9
AmTeja <krishna.teja.off@gmail.com>
2022-07-08 01:53:29 +0530

    Add project files.

---
+++ eShop/Server/Data/DataContext.cs
@@ -0,0 +1,42 @@
+﻿namespace eShop.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                    new Product
+                    {
+                        Id = 1,
+                        Title = "Where the Crawdads Sing",
+                        Description = "The reclusive Kya Clark is suspected in the death of Chase Andrews",
+                        ImageUrl = "https://images-na.ssl-images-amazon.com/images/I/51-uh7zesTL._SL300.jpg",
+                        Price = 9.99m
+                    },
+                    new Product
+                    {
+                        Id = 2,
+                        Title = "The Summer I Turned Pretty",
+                        Description = "Youth: Belly has an unforgettable summer with old friends and new experiences",
+                        ImageUrl = "https://booklist.usatoday.com/covers/isbn/9781416968290.jpg",
+                        Price = 19.99m
+                    },
+                    new Product
+                    {
+                        Id = 3,
+                        Title = "It Ends With Us",
+                        Description = "Ryle Kincaid's relationship with Lily becomes complicated when a man from her past shows up",
+                        ImageUrl = "https://images-na.ssl-images-amazon.com/images/I/51KZalxuryL._SL300.jpg",
+                        Price = 3.99m
+                    }
+                );
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ eShop/Server/Migrations/20220707201152_ProductSeeding.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eShop.Server.Migrations
+{
+    public partial class ProductSeeding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 1, "The reclusive Kya Clark is suspected in the death of Chase Andrews", "https://images-na.ssl-images-amazon.com/images/I/51-uh7zesTL._SL300.jpg", 9.99m, "Where the Crawdads Sing" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 2, "Youth: Belly has an unforgettable summer with old friends and new experiences", "https://booklist.usatoday.com/covers/isbn/9781416968290.jpg", 19.99m, "The Summer I Turned Pretty" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[] { 3, "Ryle Kincaid's relationship with Lily becomes complicated when a man from her past shows up", "https://images-na.ssl-images-amazon.com/images/I/51KZalxuryL._SL300.jpg", 3.99m, "It Ends With Us" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
